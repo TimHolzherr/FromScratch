07d3faf74d084c5f74c35d27fc0491f810f8df81
Tim Holzherr <tim.holzherr@swisslife.ch>
2021-12-16 13:41:17 +0100

    first commit

---
+++ StateMachineSolver/Program.cs
@@ -0,0 +1,10 @@
+﻿var vertices = new List<Vertex> {
+    new Vertex("A", "1", "C"),
+    new Vertex("A", "1", "B"),
+    new Vertex("B", "2", "A"),
+    new Vertex("A", "3", "C"),
+};
+
+var stateMachineSolver = new StateMachineSolver();
+
+Console.WriteLine(stateMachineSolver.Solve(vertices, new string[] { "1", "2", "3" }, "A"));
---
+++ StateMachineSolver/StateMachineSolver.cs
@@ -0,0 +1,38 @@
+public record Vertex(string Source, string Value, string Destination);
+
+public class StateMachineSolver
+{
+    Dictionary<string, List<Vertex>>? _srcDict = null;
+    public string? Solve(IList<Vertex> vertices, string[] input, string start)
+    {
+        if (input.Length == 0)
+        {
+            return start;
+        }
+        if (_srcDict == null)
+        {
+            _srcDict = vertices
+                .Select(v => v.Source)
+                .Distinct()
+                .ToDictionary(src => src, src => vertices.Where(v => v.Source == src).ToList());
+        }
+
+        if (!_srcDict.ContainsKey(start))
+        {
+            return null;
+        }
+        var possiblePaths = _srcDict[start].Where(v => v.Value == input[0]);
+
+        foreach (var path in possiblePaths)
+        {
+            var result = Solve(vertices, input[Range.StartAt(1)], path.Destination);
+            if (result != null)
+            {
+                Console.WriteLine($"{path.Source}-->{path.Value}-->{path.Destination}");
+                return result;
+            }
+        }
+        return null;
+
+    }
+}
